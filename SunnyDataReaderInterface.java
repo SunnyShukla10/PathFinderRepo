f64182dd001f75984039222d8e850befd9554302
SunnyShukla10 <140549384+SunnyShukla10@users.noreply.github.com>
2023-12-27 00:56:28 -0600

    Delete SunnyDataReaderInterface.java

--- SunnyDataReaderInterface.java
+++
@@ -1,7 +0,0 @@
-import java.io.FileNotFoundException;
-
-public interface DataReaderInterface<NodeType, EdgeType extends Number>{
-    
-    public PathNavInterface<NodeType, EdgeType> readLocationsFromFile(String fileName) throws FileNotFoundException;
-
-}
